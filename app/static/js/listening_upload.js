435d54aa9d84675417d8c5829054e28e3793942b
ZanhNe <baodoanh1102004@gmail.com>
2025-04-06 21:12:27 +0700

    tam thoi v2

--- app/static/js/listening_upload.js
+++ app/static/js/listening_upload.js
@@ -9,7 +9,7 @@ submitBtn.addEventListener("click", (e) => {
   const formData = new FormData(form);
   if (!formData.get("file") || !formData.get("audio-upload")) return;
   loader.style.display = "block";
-  fetch("http://127.0.0.1:5000/api/v1/listening/uploads", {
+  fetch("https://english.up.railway.app/api/v1/listening/uploads", {
     method: "POST",
     body: formData,
   })
